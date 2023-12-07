c801db246a897d95c115b16ccc2088001bc34748
Cody <codycabreraa@gmail.com>
2023-12-07 16:51:11 -0500

    Problema04

--- problematicas/Problema04/src/problema04/Problema04.java
+++ problematicas/Problema04/src/problema04/Problema04.java
@@ -7,11 +7,14 @@ public class Problema04 {
             for (int a = 1; a <= i; a++){
                 System.out.print("*");
             }
+            // en el system.out.print tenemos el simbolo 
             System.out.println();
+            // con este system.out.println ira agregando la cantidad de simbolos que apareceran desde 1 hasta 5 veces
         }
-        for (int i = 4; i >= 1; i--){
+        for (int i = 5; i >= 1; i--){
             for (int a = 1; a <= i; a++){
                 System.out.print("*");
+                // igualmente hara lo mismo pero en orden decreciente
             }
             System.out.println();
         }
