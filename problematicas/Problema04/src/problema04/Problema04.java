cd95068bc082e22235565b77d86babd7b56f18e6
Cody <codycabreraa@gmail.com>
2023-12-07 16:45:05 -0500

    Problema04

--- problematicas/Problema04/src/problema04/Problema04.java
+++ problematicas/Problema04/src/problema04/Problema04.java
@@ -3,14 +3,17 @@ package problema04;
 public class Problema04 {
 
     public static void main(String[] args) {
-        int a;
-        int i;
-        for (i = 1; i <= 5; i++){
-            for (a = 1; a <= i; a++){
-                System.out.println("*");
+        for (int i = 1; i <= 5; i++){
+            for (int a = 1; a <= i; a++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 4; i >= 1; i--){
+            for (int a = 1; a <= i; a++){
+                System.out.print("*");
             }
             System.out.println();
         }
     }
-    
 }
